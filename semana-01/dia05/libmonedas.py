af55c46f42bd0a68d942e95b1a1562c61fe91de3
David Rivera Robles <odriverar@hotmail.com>
2022-04-18 21:33:40 -0500

    Ultima clase semana 01 version final

--- semana-01/dia05/libmonedas.py
+++ semana-01/dia05/libmonedas.py
@@ -1,27 +1,42 @@
 class moneda:
-    def __init__(self, tipo, cambio):
+    
+    def __init__(self,tipo,camSoles):
         self.tipo = tipo
-        self.cambio = cambio
+        self.cambioSoles = camSoles
         
     def mostrar(self):
-        print("La moneda es: ", self.tipo, " y su cambio a Soles es: ", str(self.cambio))
-    
-    def convertir(self, monConvertir, tipo):
-        if tipo == "soles":
-            resultado = monConvertir * self.cambio
-        else:
-            resultado = monConvertir / self.cambio
+        print("la moneda es " + self.tipo + " y su tipo de cambio a soles es " + str(self.cambioSoles))
         
+    def convertir(self,monto,origen):
+        if(origen == "soles"):
+            resultado = monto * self.cambioSoles
+        else:
+            resultado =  monto / self.cambioSoles
         return resultado
+        
 
+monedaDolares = moneda('dolares',3.71)
+monedaEuros = moneda('euros',4.10)
+
+monedaDolares.mostrar()
+monedaEuros.mostrar()
+#ENTRADAS
+origen = input("ingrese moneda origen:")
+destino = input("Ingrese moneda destino:")
+montoOrigen = float(input("ingrese el monto : "))
+
+#PROCESO
+if(origen == "soles" and destino ==  "dolares"):
+    resultado = monedaDolares.convertir(montoOrigen,destino)
+elif(origen == "soles" and destino ==  "euros"):
+    resultado = monedaEuros.convertir(montoOrigen,destino)
+elif(origen == "dolares" and destino == "soles"):
+    resultado = monedaDolares.convertir(montoOrigen,destino)
+elif(origen == "euros" and destino == "soles"):
+    resultado = monedaEuros.convertir(montoOrigen.destino)
     
-monedaDolar = moneda("Dolar $", 3.76)
-monedaEuro = moneda("Euro €", 4.10)
-
-monedaDolar.mostrar()
-monedaEuro.mostrar()
-
-tipo = input("Ingrese el tipo de moneda a convertir: ")
-monOrigen = float(input("Ingrese el monto: "))
-opcion = input("Ingrese la opcion para convertir 1) soles a " + tipo + " 2) de " + tipo + " a soles: ")
-
+#SALIDAS
+print(" ===================================== ")
+print(" monto origen : " + str(montoOrigen) + " " + str(origen))
+print(" moneda destino : " + str(destino))
+print(" resultado : " + str(resultado) + " " + str(destino))
