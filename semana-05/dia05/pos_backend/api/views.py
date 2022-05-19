3caa685fff3efb657ce4b9edf0cdcef8ce0d606b
Oswaldo David Rivera Robles <david@PC-David.local>
2022-05-19 05:10:56 -0500

    Update uno, dia 5 semana 5

---
+++ semana-05/dia05/pos_backend/api/views.py
@@ -0,0 +1,85 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import (
+      Categoria, Mesa, Pedido, Plato
+)
+
+from .serializers import (
+      CategoriaSerializer,
+      MesaSerializer,
+      PedidoSerializerPOST,
+      PlatoSerializer,
+      CategoriaPlatosSerializer,
+      PedidoPlatoSerializerPOST
+)
+
+class IndexView(APIView):
+      def get(self, request):
+            context ={
+                  'ok': True,
+                  'message':'¡El servidor está activo!'
+            }
+            return Response(context)
+      
+class CategoriaView(APIView):
+      def get(self, request):
+            dataCategoria = Categoria.objects.all()
+            serCategoria = CategoriaSerializer(dataCategoria, many=True)
+            
+            context = {
+                  'ok': True,
+                  'content': serCategoria.data
+            }
+            
+            return Response(context)
+      
+class MesaView(APIView):
+      def get(self, request):
+            dataMesa = Mesa.objects.all()
+            serMesa = MesaSerializer(dataMesa, many=True)
+            
+            context = {
+                  'ok': True,
+                  'content': serMesa.data
+            }
+            
+            return Response(context)
+      
+class PlatoView(APIView):
+      def get(self, request):
+            dataPlato = Plato.objects.all()
+            serPlato = PlatoSerializer(dataPlato, many=True)
+            
+            context = {
+                  'ok': True,
+                  'content': serPlato.data
+            }
+            
+            return Response(context)
+
+class CategoriaPlatosView(APIView):
+      def get(self, request, categoria_id):
+            dataCategoria = Categoria.objects.get(pk=categoria_id)
+            serCategoriaPlatos = CategoriaPlatosSerializer(dataCategoria)
+            
+            context ={
+                  'ok': True,
+                  'content': serCategoriaPlatos.data
+            }
+            
+            return Response(context)
+      
+class PedidoView(APIView):
+      
+    def post(self,request):
+        serPedido = PedidoSerializerPOST(data=request.data)
+        serPedido.is_valid(raise_exception=True)
+        serPedido.save()
+
+        context = {
+            'ok':True,
+            'content':serPedido.data
+        }
+        
+        return Response(context)
